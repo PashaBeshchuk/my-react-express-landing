e50280ff056998fd1ee5082d21bdae96a5da8ebe
Pavlo_Beshchuk <rocaroca@MacBook-Pro-Roca.local>
2025-09-04 21:39:29 +0300

    Initial commit: React + Express project

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors()); 
+app.use(express.json());
+
+app.get('/api/temperature', (req, res) => {
+  res.json({ temperature: Math.round((Math.random() * 70 - 20) * 10) / 10 });
+});
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    });
+  }
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
