a4d221b90d2dc91e63508364423ecac6fdefd20d
Pavlo_Beshchuk <rocaroca@MacBook-Pro-Roca.local>
2025-09-05 19:33:35 +0300

    Add server and client

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors()); 
 app.use(express.json());
@@ -11,12 +11,12 @@ app.get('/api/temperature', (req, res) => {
   res.json({ temperature: Math.round((Math.random() * 70 - 20) * 10) / 10 });
 });
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-    });
-  }
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
